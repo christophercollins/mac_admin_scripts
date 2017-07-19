213eccbba389a9e209f3ce4b286d1f3991886ee8
Christopher Collins <badlittlerobots@gmail.com>
2017-07-19 15:44:38 -0700

    added set_dnd.py

---
+++ enable_dnd_in_notificationcenter/set_dnd.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from CoreFoundation import CFPreferencesSetValue, CFPreferencesSynchronize
+from Foundation import NSDate
+import subprocess
+
+def set_value(key, value, domain):
+    CFPreferencesSetValue(key, value, domain, 'kCFPreferencesCurrentUser', 'kCFPreferencesCurrentHost')
+    
+def main():
+    the_date = NSDate.date()
+    set_value('doNotDisturb', True, 'com.apple.notificationcenterui')
+    set_value('doNotDisturbDate', the_date, 'com.apple.notificationcenterui')
+    CFPreferencesSynchronize('com.apple.notificationcenterui', 'kCFPreferencesCurrentUser', 'kCFPreferencesAnyHost')
+    subprocess.call(['killall', 'NotificationCenter'])
+
+if __name__ == '__main__':
+    main()
